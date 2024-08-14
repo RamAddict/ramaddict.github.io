8d67ce8dc12ba7ec5e4df06b26f6d6c796967de7
Arthur <apbiancofriber@gmail.com>
2024-08-13 21:45:07 -0300

    add nuxt base

---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  devServer: {
+    port: 4501
+  },
+  app: {
+    baseURL: 'https://ramaddict.github.io/'
+  }
+})
