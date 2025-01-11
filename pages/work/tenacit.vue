2f3d1746ad3429f8c632510742d41ac90f770b8c
RamAddict <32513434+RamAddict@users.noreply.github.com>
2025-01-11 20:22:21 -0300

    Update tenacit.vue

--- pages/work/tenacit.vue
+++ pages/work/tenacit.vue
@@ -11,22 +11,22 @@
         <span class="font-cursive text-6xl">T</span>enacit is a startup I co-founded during my time
         at BRy Tecnologia. It is a maintenance management platform targeted at small and medium
         enterprises. Having worked at BRy for half a year, I was anxious to try out something other
-        than a Spring monolith running on a self hosted kubernetes. So that's what I did.
+        than a Spring monolith running on self-hosted Kubernetes. So that's what I did.
       </p>
       <p>
         We settled for a serverless architecture, using AWS as the cloud provider. This solution
-        proved to be quite effective and cost efficient. It runs basically for free, all three
-        environments, really. Our biggest costs are the hosted zone and the domain name. It didn't
-        come with its own issues, though, development does take a bit longer as we have to test in
+        proved to be quite effective and cost-efficient. It runs almost for free. All three
+        environments. Our biggest costs are the hosted zone and the domain name. It didn't
+        come with its issues, though, development does take a bit longer as we have to test in
         the cloud and also work around some issues inherent to the architecture. Such as creating
         warmers for the lambdas and dealing with SNS topics so we don't freeze the user while
         sending emails, just to name a few.
       </p>
       <p>
         As usual, I operated in all facets of the project, from coming up with UI sketches and
-        implemmenting them to creating and testing the api in the backend to coming up with and
-        writing the aws infraestructure. The project was built with Angular and Angular Material in
-        the frontend, and Express running in lambda and a mongoDB database.
+        implementing them to creating and testing the API in the backend to coming up with and
+        writing the AWS infrastructure. The project was built with Angular and Angular Material in
+        the front, and Express running in lambda and a MongoDB database.
       </p>
       <p>
         This was the best learning experience I had in my career so far. I learned a lot about
